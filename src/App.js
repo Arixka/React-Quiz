bb808fad30f4139cecdb87886b62b2828c3103dc
arixka <marisiver25@gmail.com>
2021-08-21 14:04:15 +0100

    quizapi component quiz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import "./App.css"
-import { useState, useContext } from "react"
-import MainMenu from "./components/MainMenu"
-import EndScream from "./components/EndScream"
-import Quiz from "./components/Quiz"
-import { QuizContext } from "./helpers/QuizContext"
+import './App.css'
+import { useState, useContext } from 'react'
+import MainMenu from './components/MainMenu'
+import EndScream from './components/EndScream'
+import Quiz from './components/Quiz'
+import { QuizContext } from './helpers/QuizContext'
 
 function App() {
-	const [gameState, setGameState] = useState("menu")
+	const [gameState, setGameState] = useState('menu')
 
 	return (
-		<div className="App">
-			<h1>
-				Quiz App
-			</h1>
+		<div className='App'>
+			<h1>Quiz App</h1>
 
 			<QuizContext.Provider value={{ gameState, setGameState }}>
-				{gameState === "menu" && <MainMenu />}
-				{gameState === "quiz" && <Quiz />}
-				{gameState === "finish" && <EndScream />}
+				{gameState === 'menu' && <MainMenu />}
+				{gameState === 'quiz' && <Quiz />}
+				{gameState === 'finish' && <EndScream />}
 			</QuizContext.Provider>
 		</div>
 	)
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,12 +4,17 @@ import { QuizApi } from "../helpers/QuizApi"
 const Quiz = () => {
 	const [currQuestion, setcurrQuestion] = useState(0)
 
-	console.log(QuizApi[currQuestion])
-  const {question} =QuizApi[currQuestion]
-
+	const { question, incorrect_answers, correct_answer } = QuizApi[currQuestion]
+	console.log(correct_answer)
+	const answers = [...incorrect_answers, correct_answer]
 	return (
 		<div className="quiz">
-			<h3 >{question}</h3>
+			<h3>{question}</h3>
+			<ul>
+				{answers.map((elem) => (
+					<button key={elem}>{elem}</button>
+				))}
+			</ul>
 		</div>
 	)
 }
